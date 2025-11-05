60396da9873bde772554b4785dad89cc163d425f
Ximera5185 <48877880+Ximera5185@users.noreply.github.com>
2025-11-05 23:21:03 +0800

    1
    1

--- Alarm/Assets/Scripts/Сharacters/Player/GroundDetector.cs
+++ Alarm/Assets/Scripts/Сharacters/Player/GroundDetector.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GroundDetector : MonoBehaviour
@@ -5,7 +7,39 @@ public class GroundDetector : MonoBehaviour
     [SerializeField] private LayerMask _groundMask;
 
     [SerializeField] private float _groundDistance = 0.1f;
+    [SerializeField] private float _sphereRadius = 0.4f;
+    [SerializeField] private float _heightOffset = 5f;
 
-     public bool IsGrounded => Physics.Raycast(transform.position, Vector3.down, _groundDistance, _groundMask);
-   // public bool IsGrounded => Physics.SphereCast(transform.position,);
+    [SerializeField] private float capsuleHeight = 2f; // Высота капсулы
+    [SerializeField] private float capsuleRadius = 0.5f; // Радиус капсулы
+                                                         //public bool IsGrounded => Physics.CapsuleCast(transform.position, Vector3.down, _groundDistance, _groundMask);
+
+    // public bool IsGrounded => Physics.SphereCast(transform.position + Vector3.up * _heightOffset , _sphereRadius, Vector3.down, out RaycastHit hit, _groundDistance, _groundMask);
+
+    /* private void OnDrawGizmos()
+     {
+         // Устанавливаем цвет Gizmos (например, зеленый для "земли")
+         Gizmos.color = Color.green;
+
+         // Рисуем сферу в позиции объекта с заданным радиусом
+         Gizmos.DrawWireSphere(transform.position + Vector3.up * _heightOffset, _sphereRadius);
+
+         // Рисуем линию вниз, чтобы визуализировать направление SphereCast
+         Gizmos.DrawLine(transform.position + Vector3.up * _heightOffset, transform.position + Vector3.down * (_groundDistance + _sphereRadius));
+     }*/
+    
+
+    public bool IsGrounded => Physics.CapsuleCast(transform.position + Vector3.up * (capsuleHeight / 2 + capsuleRadius), transform.position + Vector3.up * (capsuleHeight / 2 - capsuleRadius), capsuleRadius, Vector3.down, out RaycastHit hit, _groundDistance, _groundMask);
+
+    private void OnDrawGizmos()
+    {
+        // Визуализируем капсулу в редакторе
+        Gizmos.color = Color.green;
+        Vector3 bottom = transform.position + Vector3.up * (capsuleHeight / 2 - capsuleRadius);
+        Vector3 top = transform.position + Vector3.up * (capsuleHeight / 2 + capsuleRadius);
+        Gizmos.DrawWireSphere(top, capsuleRadius);
+        Gizmos.DrawWireSphere(bottom, capsuleRadius);
+        Gizmos.DrawLine(top, bottom);
+        
+    }
 }
--- Alarm/Assets/Scripts/Сharacters/Player/KeybordMouseInputReader.cs
+++ Alarm/Assets/Scripts/Сharacters/Player/KeybordMouseInputReader.cs
@@ -87,6 +87,4 @@ public class KeybordMouseInputReader : MonoBehaviour,IinputReader
             OnSprintStop?.Invoke();
         }
     }
-
-   
 }
